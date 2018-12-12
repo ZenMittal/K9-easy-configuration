4d47f2606cef7b406ad91ae32640e8ce6bd86f09
Shubham <bizmailsam@gmail.com>
2018-12-12 14:15:40 +0530

    added files

---
+++ block.js
@@ -0,0 +1,17 @@
+[
+
+"https://www.revouninstaller.com",
+"https://filehippo.com/download_revo_uninstaller_pro/",
+"https://www.torproject.org",
+"https://tor-browser.en.softonic.com/",
+"https://www.techspot.com/downloads/5183-tor-browser.html",
+"https://github.com/TheTorProject/gettorbrowser",
+"https://download.cnet.com/mac/the-tor-project/3260-20_4-10192562-1.html",
+"https://revo-uninstaller-pro.en.uptodown.com/windows",
+"https://download.cnet.com/Revo-Uninstaller/3000-2096_4-10687648.html",
+"https://www.reddit.com/",
+"https://www.imgur.com/",
+"https://www.bing.com/images",
+"https://www.bing.com/videos"
+
+]
